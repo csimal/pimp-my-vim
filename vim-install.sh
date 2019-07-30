cefdf559b290083915eb822ea64dc281f72ce9d5
csimal <csimal@student.unamur.be>
2019-07-30 15:26:49 +0200

    added fzf installation

--- vim-install.sh
+++ vim-install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "Installing minpac:"
+echo "Installing minpac. Make sure you're using Vim 8 or later."
 mkdir -p ~/.vim/pack/minpac/opt
 cd ~/.vim/pack/minpac/opt
 git clone https://github.com/k-takata/minpac.git
 echo "minpac is now installed. Reload vim and run :PackUpdate to install packages."
+echo "Installing fzf"
+cd ~/.vim/pack/bundle/start
+git clone https://github.com/junegunn/fzf
+~/.vim/pack/bundle/start/fzf/install --bin
+echo "Remember to add 'export PATH=\$PATH:~/.vim/pack/bundle/start/fzf/bin' to your .bashrc"
